4ba3c4a397674efdd35e0c6b0c53147eb3a365ae
Vincent <antoniod877@gmail.com>
2023-05-01 04:52:19 +0200

    Update main.py
    refactoring and changing the way calculations are done may finish it later

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from math import *
 from pygame import *
+import asyncio
 
 PI = 3.1415926535
 
@@ -16,7 +17,15 @@ class Ray:
         self.COS_LATITUDE = cos(latitude)
         self.COS_LONGITUDE = cos(longitude)
 
+        self.nbrSteps = 0
+        self.collision = False
+        self.sphereInstance = None
 
+
+    def setInformations(self, nbrSteps, collision, sphereInstance):
+        self.nbrSteps = nbrSteps
+        self.collision = collision
+        self.sphereInstance = sphereInstance
 
     def stepRay(self,step):
         self.x += self.COS_LONGITUDE * self.COS_LATITUDE * step
@@ -43,7 +52,7 @@ class Sphere:
         return sqrt( (x-self.x)**2 + (y-self.y)**2 + (z-self.z)**2 ) - self.r
     
     def getNormal(self,x,y,z):
-        return (x-self.x,y-self.y,z-self.z)
+        return Vector3(x-self.x,y-self.y,z-self.z).normalize()
     
     def getMaterial(self):
         return self.material
@@ -70,32 +79,45 @@ class Camera:
         self.width = width
         self.height = height
         
-
+    #TODO shoot ray from full position and not just screen space 
     def shootRay(self,scene,pixelX,pixelY):
 
-        longitude = pixelX * PI / self.width + self.phi
-        latitude = pixelY * PI / self.height + self.theta
+        ratio = self.width / self.height
 
-        rayon = Ray(self.x,self.y,self.z,longitude,latitude)
-        nbPas = 0
-        (stepSize, sphereInstance) = scene.getDistToScene(rayon.x,rayon.y,rayon.z)
+        longitude = pixelX * PI / self.width + self.phi
+        latitude = pixelY * PI / (self.height * ratio) + self.theta
+
+        ray = Ray(self.x,self.y,self.z,longitude,latitude)
+        nbrSteps = 0
+        (stepSize, sphereInstance) = scene.getDistToScene(ray.x,ray.y,ray.z)
         collision = stepSize < self.maxSize
 
-        while nbPas <= 10 and not collision:
-            rayon.stepRay(stepSize)
-            (stepSize, sphereInstance) = scene.getDistToScene(rayon.x,rayon.y,rayon.z)
-            nbPas +=1
+        while nbrSteps <= 10 and not collision:
+            ray.stepRay(stepSize)
+            (stepSize, sphereInstance) = scene.getDistToScene(ray.x,ray.y,ray.z)
+            nbrSteps +=1
             collision = stepSize < self.maxSize
-        return (collision,nbPas,sphereInstance)
-    
+
+        ray.setInformations(nbrSteps,collision,sphereInstance)
+        return ray
+       
             
 
 class Scene:
 
-    def __init__(self):
-        self.spheres = [Sphere(15,5,-3,10, Material(0.5,0.5,(255,0,0))),
-                        Sphere(15,-5,-3,10, Material(0.5,0.5,(0,255,0))),
-                        Sphere(15,0,3,10, Material(0.5,0.5,(0,0,255))),]
+    def __init__(self, width, height):
+        self.spheres = [Sphere(15,-45,0,25, Material(0.5,0.5,(0,255,0))),
+                        Sphere(15,45,-0,25, Material(0.5,0.5,(0,0,255)))]
+
+        self.camera = Camera(0,0,0,width,height,0,0)
+        self.width = width
+        self.height = height
+
+        assert width > 0 and height > 0 and width % 2 == 0 and height % 2 == 0, "Width and height must be even and positive"
+
+        init()
+        self.window = display.set_mode((width,height))
+        self.window.fill((0,0,0))
 
     def getDistToScene(self,x,y,z):
         minDist = self.spheres[0].distanceTo(x,y,z)
@@ -112,25 +134,30 @@ class Scene:
         return (minDist, sphereInst)
 
     def render(self):
-        bismillahCam = Camera(0,0,0,200,200,0,0)
-        for x in range(-100,100):
-            for y in range(-100,100):
-                (collision,nbPas,sphereInstance) = bismillahCam.shootRay(self,x,y)
-                if collision:
-                    window.set_at((x+100,y+100),sphereInstance.getColor())
+        
+        halfWidth = self.width//2
+        halfHeight = self.height//2
 
+        
 
-init()
-window = display.set_mode((200,200))
+        for x in range(-halfWidth,halfWidth):
+            for y in range(-halfHeight,halfHeight):
+                ray = self.camera.shootRay(self,x,y)
+                if ray.collision:
+                    vector = ray.sphereInstance.getNormal(ray.x,ray.y,ray.z)
+                    vector2 = vector.cross(Vector3(ray.x, ray.y, ray.z))
+                    ray2 = self.camera.shootRay(vector2.x,vector2.y,vector2.z)
+                    self.window.set_at((x+halfWidth,y+halfHeight), ray2.sphereInstance.getColor())
+            display.flip()
 
-window.fill((0,0,0))
-scene = Scene()
+    def wait(self):
+        running = True
+        while running:
+            for evt in event.get():
+                if evt.type == QUIT:
+                    running = False
+        quit()
+
+scene = Scene(300,300)
 scene.render()
-
-display.flip()
-running = True
-while running:
-    for evt in event.get():
-        if evt.type == QUIT:
-            running = False
-quit()
+scene.wait()
